063a023c3131c0a4fdf3c3c969a42d185fe3544c
leffred <lefebvre.frederic@gmail.com>
2025-12-11 12:23:02 +0100

    Finalisation SEO Technique : Sitemap et Robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,16 @@
-// @ts-check
+﻿// @ts-check
 import { defineConfig } from 'astro/config';
-
 import node from '@astrojs/node';
-
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://jourdechance.com', // Ton futur domaine officiel
+  integrations: [sitemap()],
   adapter: node({
     mode: 'standalone'
   }),
-
   vite: {
     plugins: [tailwindcss()]
   }
